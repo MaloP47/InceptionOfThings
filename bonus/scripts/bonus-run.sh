87e03bed3b18ccb33f43a9c4c879101d3efe7d3f
Malo <mpeulet@gmail.com>
2025-03-03 23:36:57 +0100

    bonus worked, need to improve everything

--- bonus/scripts/bonus-run.sh
+++ bonus/scripts/bonus-run.sh
@@ -1,16 +1,29 @@
 # #!/usr/bin/env bash
 
-k3d cluster create p3 -p "7777:80@loadbalancer" -p "8888:8888@loadbalancer"
+k3d cluster create bonus -p "7777:80@loadbalancer" -p "8888:8888@loadbalancer" -p "9999:9999@loadbalancer"
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl wait --for=condition=ready --timeout=600s pod --all -n argocd
 
-git clone https://github.com/MaloP47/IOT-mpeulet.git
+if [ ! -d "IOT-mpeulet" ]; then
+    git clone https://github.com/MaloP47/IOT-mpeulet.git
+fi
+
 sleep 2
 kubectl apply -f IOT-mpeulet/application.yaml
 sleep 2
 kubectl wait --for=condition=ready --timeout=600s pod --all -n dev
+
+
+if [ ! -d "iot-gitlab" ]; then
+	git clone http://iot-gitlab:8929/root/test-iot.git
+fi
+
+sleep 2
+kubectl apply -f test-iot/application.yaml
+sleep 2
+kubectl wait --for=condition=ready --timeout=600s pod --all -n gitlab
 
 kubectl apply -f ./confs/Ingress/argoCD-Ingress.yaml
 sleep 2
@@ -28,3 +41,7 @@ ngrok http --url=fair-flounder-completely.ngrok-free.app 8080 --host-header=loca
 # k3d cluster delete p3
 # rm mdp
 # rm -rf IOT-mpeulet
+
+# règle pour pull les repo
+# changer l'url dans github sur p3
+# script install gitlab
